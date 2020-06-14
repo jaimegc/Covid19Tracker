854337d1d56782644b40ae6bacbb32e3e3b27367
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-06-14 19:49:31 +0200

    Remove unused methods to load database

--- app/src/main/java/com/jaimegc/covid19tracker/data/room/CovidTrackerDatabase.kt
+++ app/src/main/java/com/jaimegc/covid19tracker/data/room/CovidTrackerDatabase.kt
@@ -4,16 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
 import com.jaimegc.covid19tracker.data.room.daos.*
 import com.jaimegc.covid19tracker.data.room.entities.*
 import com.jaimegc.covid19tracker.data.room.views.CountryAndStatsDV
 import com.jaimegc.covid19tracker.data.room.views.RegionAndStatsDV
-import com.jaimegc.covid19tracker.worker.PopulateDatabaseWorker
 import java.io.File
-
 
 @Database(entities = [CountryEntity::class, WorldStatsEntity::class, CountryStatsEntity::class,
     RegionEntity::class, RegionStatsEntity::class, SubRegionEntity::class, SubRegionStatsEntity::class],
@@ -35,16 +30,7 @@ abstract class Covid19TrackerDatabase : RoomDatabase() {
 
         fun build(context: Context): Covid19TrackerDatabase =
             Room.databaseBuilder(context, Covid19TrackerDatabase::class.java, DATABASE_NAME)
-                //.createFromAsset("covid19-tracker-db")
                 .createFromFile(File("${context.filesDir}${File.separator}$DATABASE_NAME"))
-                .addCallback(object : RoomDatabase.Callback() {
-                    override fun onCreate(db: SupportSQLiteDatabase) {
-                        super.onCreate(db)
-                        // Populate database using a Worker
-                        //val request = OneTimeWorkRequestBuilder<PopulateDatabaseWorker>().build()
-                        //WorkManager.getInstance(context).enqueue(request)
-                    }
-                })
                 .fallbackToDestructiveMigration()
                 .build()
     }
