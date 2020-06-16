1d9f9888b11c598c7bf821e96d5f27f108a7b0ca
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-06-16 16:45:16 +0200

    Add percentage as a constant

--- app/src/main/java/com/jaimegc/covid19tracker/common/extensions/NumberExtensions.kt
+++ app/src/main/java/com/jaimegc/covid19tracker/common/extensions/NumberExtensions.kt
@@ -5,6 +5,8 @@ import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.Locale
 
+const val PERCENTAGE = 100
+
 fun Double.formatDecimals(): String = numberFormatDecimals(this).format(this)
 
 fun Long.formatValue(): String = numberFormat().format(this)
@@ -12,10 +14,10 @@ fun Long.formatValue(): String = numberFormat().format(this)
 fun Float.formatValue(): String = numberFormat().format(this)
 
 fun Double.percentage(value: Double): String =
-    "${(this / value * 100).formatDecimals()}%"
+    "${(this / value * PERCENTAGE).formatDecimals()}%"
 
 fun Float.percentage(value: Float): String =
-    "${(this / value * 100).toDouble().formatDecimals()}%"
+    "${(this / value * PERCENTAGE).toDouble().formatDecimals()}%"
 
 private fun numberFormatDecimals(value: Double, totalDecimals: Int = 2): NumberFormat {
     val locale = Locale.getDefault()
