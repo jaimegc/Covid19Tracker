e71d67c37a0337121783752a3eb9a9c571c35871
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-10-10 12:19:50 +0200

    Add compact big number extension

--- app/src/main/java/com/jaimegc/covid19tracker/common/extensions/NumberExtensions.kt
+++ app/src/main/java/com/jaimegc/covid19tracker/common/extensions/NumberExtensions.kt
@@ -4,14 +4,18 @@ import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.Locale
+import kotlin.math.ln
 
 private const val PERCENTAGE = 100
+private const val MILLION = 1000000.0
 
 fun Double.formatDecimals(): String = numberFormatDecimals(this).format(this)
 
 fun Long.formatValue(): String = numberFormat().format(this)
 
 fun Float.formatValue(): String = numberFormat().format(this)
+
+fun Long.formatCompactValue(): String = compactValue(this)
 
 fun Double.percentage(): String = (this * PERCENTAGE).formatDecimals()
 
@@ -27,12 +31,20 @@ private fun numberFormatDecimals(value: Double, totalDecimals: Int = 2): NumberF
     result.minimumFractionDigits = totalDecimals
     result.maximumFractionDigits = totalDecimals
 
-    result.roundingMode = if (value >= 1.0) RoundingMode.HALF_UP else RoundingMode.CEILING
+    result.roundingMode = if (value >= 1) RoundingMode.HALF_UP else RoundingMode.CEILING
 
     if (result is DecimalFormat && totalDecimals > 0) result.isDecimalSeparatorAlwaysShown = true
 
     return result
 }
 
-private fun numberFormat(): NumberFormat =
-    NumberFormat.getInstance(Locale.getDefault())
+private fun compactValue(value: Long): String {
+    if (value < MILLION) return numberFormat().format(value)
+    val exp = (ln(value.toDouble()) / ln(MILLION)).toInt()
+    val compactValue = value.toDouble() / MILLION
+    val format = numberFormatDecimals(value = compactValue, totalDecimals = 2).format(compactValue)
+
+    return "$format${"MGTPE"[exp - 1]}"
+}
+
+private fun numberFormat(): NumberFormat = NumberFormat.getInstance(Locale.getDefault())
