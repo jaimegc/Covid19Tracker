aa7b64139dc9780f4b29ad18a6a731c7da4832fc
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-10-11 13:01:49 +0200

    Add empty methods

---
+++ app/src/test/java/com/jaimegc/covid19tracker/mapper/DomainMapperTest.kt
@@ -0,0 +1,218 @@
+package com.jaimegc.covid19tracker.mapper
+
+import org.junit.Before
+import org.junit.Test
+import java.util.*
+
+class DomainMapperTest {
+
+    @Before
+    fun setup() {
+        Locale.setDefault(Locale.US)
+    }
+
+    @Test
+    fun covidTrackerDtoToDomain() {
+
+    }
+
+    @Test
+    fun covidTrackerTotalDtoToDomain() {
+
+    }
+
+    @Test
+    fun worldAndCountriesStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun countryAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun regionAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun subRegionAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun listCountryEntityToDomain() {
+
+    }
+
+    @Test
+    fun listRegionEntityToDomain() {
+
+    }
+
+    @Test
+    fun listCountryAndOneStatsPojoToPojoCountriesOrdered() {
+
+    }
+
+    @Test
+    fun listRegionAndOneStatsPojoToPojoRegionsOrdered() {
+
+    }
+
+    @Test
+    fun listSubRegionAndOneStatsPojoToPojoSubRegionsOrdered() {
+
+    }
+
+    @Test
+    fun CountryAndOneStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun RegionAndOneStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun listWorldStatsEntityToDomain() {
+        
+    }
+
+    @Test
+    fun worldStatsEntityToDomain() {
+        
+    }
+
+    @Test
+    fun listCountryAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun listRegionAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun listSubRegionAndStatsPojoToDomain() {
+
+    }
+
+    @Test
+    fun listCountryStatsEntityToStatsDomain() {
+
+    }
+
+    @Test
+    fun listRegionStatsEntityToStatsDomain() {
+
+    }
+
+    @Test
+    fun listSubRegionAndStatsDVToDomain() {
+
+    }
+
+    @Test
+    fun listRegionAndStatsDVToDomain() {
+
+    }
+
+    @Test
+    fun listRegionAndStatsPojoToPojoRegionDomain() {
+
+    }
+
+    @Test
+    fun listSubRegionAndStatsPojoToPojoSubRegionDomain() {
+
+    }
+
+    @Test
+    fun listRegionStatsEntityToRegionDomain() {
+
+    }
+
+    @Test
+    fun listSubRegionStatsEntityToSubRegionDomain() {
+
+    }
+
+    @Test
+    fun regionStatsEntityToDomain() {
+
+    }
+
+    @Test
+    fun subRegionStatsEntityToDomain() {
+
+    }
+
+    @Test
+    fun countryEntityToDomain() {
+
+    }
+
+    @Test
+    fun regionEntityToDomain() {
+
+    }
+
+    @Test
+    fun subRegionEntityToDomain() {
+
+    }
+
+    @Test
+    fun statsEmbeddedToDomain() {
+
+    }
+
+    @Test
+    fun countryOneStatsToEntity() {
+
+    }
+
+    @Test
+    fun worldStatsToEntity() {
+
+    }
+
+    @Test
+    fun statsToEmbedded() {
+
+    }
+
+    @Test
+    fun statsToEntity() {
+
+    }
+
+    @Test
+    fun countryStatsEntityToDomain() {
+
+    }
+
+    @Test
+    fun regionToEntity() {
+
+    }
+
+    @Test
+    fun subRegionToEntity() {
+
+    }
+
+    @Test
+    fun regionStatsToEntity() {
+
+    }
+
+    @Test
+    fun subRegionStatsToEntity() {
+
+    }
+}
