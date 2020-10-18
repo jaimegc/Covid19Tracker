22f5edcb3270a4bc04d2f853a6144edf3c6e5992
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-10-18 22:29:31 +0200

    Add new main view model tests

--- app/src/test/java/com/jaimegc/covid19tracker/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/jaimegc/covid19tracker/viewmodel/MainViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.jaimegc.covid19tracker.viewmodel
 
 import arrow.core.Either
+import com.jaimegc.covid19tracker.data.repository.CovidTrackerRepository
 import com.jaimegc.covid19tracker.domain.model.CovidTracker
 import com.jaimegc.covid19tracker.domain.model.DomainError
 import com.jaimegc.covid19tracker.domain.states.State
@@ -8,32 +9,80 @@ import com.jaimegc.covid19tracker.domain.states.StateError
 import com.jaimegc.covid19tracker.domain.usecase.GetCovidTracker
 import com.jaimegc.covid19tracker.ui.home.MainViewModel
 import com.jaimegc.covid19tracker.utils.ModelBuilder.covidTracker
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import dev.olog.flow.test.observer.test
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
 class MainViewModelTest {
 
     private lateinit var mainViewModel: MainViewModel
-    private val getCovidTracker: GetCovidTracker = mock()
+    private lateinit var getCovidTracker: GetCovidTracker
+    private val repository: CovidTrackerRepository = mock()
 
     private val stateLoading: State<CovidTracker> = State.Loading()
-    private val stateErrorDatabaseEmpty: StateError<DomainError> = StateError.Error(DomainError.DatabaseEmptyData)
+    private val stateErrorDatabaseEmpty: StateError<DomainError> =
+        StateError.Error(DomainError.DatabaseEmptyData)
     private val stateSuccess: State<CovidTracker> = State.Success(covidTracker)
 
     @Before
     fun setup() {
+        getCovidTracker = GetCovidTracker(repository)
         mainViewModel = MainViewModel(getCovidTracker)
     }
 
     @Test
-    fun getCovidTrackerByDateReturnLoadingAndSuccess() = runBlockingTest {
+    fun getCovidTrackerByDateReturnLoadingAndSuccessIfDateExists() = runBlockingTest {
+        val useCase = mock<GetCovidTracker>() {
+            onBlocking { getCovidTrackerByDate() } doReturn flow {
+                emit(Either.right(stateLoading))
+                delay(10)
+                emit(Either.right(stateSuccess))
+            }
+        }
+
+        val flow = useCase.getCovidTrackerByDate()
+
+        flow.collectIndexed { index, data ->
+            when (index) {
+                0 -> assertEquals(data, Either.right(stateLoading))
+                1 -> assertEquals(data, Either.right(stateSuccess))
+            }
+        }
+    }
+
+    @Test
+    fun getCovidTrackerByDateReturnLoadingAndErrorDatabaseEmptyIfDoesntDateExist() = runBlockingTest {
+        val useCase = mock<GetCovidTracker>() {
+            onBlocking { getCovidTrackerByDate() } doReturn flow {
+                emit(Either.right(stateLoading))
+                delay(10)
+                emit(Either.left(stateErrorDatabaseEmpty))
+            }
+        }
+
+        val flow = useCase.getCovidTrackerByDate()
+
+        flow.collectIndexed { index, data ->
+            when (index) {
+                0 -> assertEquals(data, Either.right(stateLoading))
+                1 -> assertEquals(data, Either.left(stateErrorDatabaseEmpty))
+            }
+        }
+    }
+
+    @Test
+    fun getCovidTrackerByDateReturnLoadingAndSuccessIfDateExistsUsingFlowTest() = runBlockingTest {
         val flow: Flow<Either<StateError<DomainError>, State<CovidTracker>>> = flow {
             emit(Either.right(stateLoading))
             delay(10)
@@ -50,7 +99,7 @@ class MainViewModelTest {
     }
 
     @Test
-    fun getCovidTrackerByDateReturnLoadingAndErrorDatabaseEmpty() = runBlockingTest {
+    fun getCovidTrackerByDateReturnLoadingAndErrorDatabaseEmptyIfDoesntDateExistUsingFlowTest() = runBlockingTest {
         val flow: Flow<Either<StateError<DomainError>, State<CovidTracker>>> = flow {
             emit(Either.right(stateLoading))
             delay(10)
@@ -64,5 +113,12 @@ class MainViewModelTest {
             assertValueCount(2)
             assertComplete()
         }
+    }
+
+    @Test
+    fun getCovidTrackerByDateShouldCallRepository() = runBlockingTest {
+        getCovidTracker.getCovidTrackerByDate()
+
+        verify(repository).getCovidTrackerByDate(any())
     }
 }
