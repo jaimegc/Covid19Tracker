bad2fc89aaa5118899c68691118af0dbdd32e5b7
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-11-02 18:41:32 +0100

    Add country & region tests

---
+++ app/src/test/java/com/jaimegc/covid19tracker/viewmodel/CountryViewModelTest.kt
@@ -0,0 +1,189 @@
+package com.jaimegc.covid19tracker.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import arrow.core.Either
+import com.jaimegc.covid19tracker.domain.usecase.GetCountry
+import com.jaimegc.covid19tracker.domain.usecase.GetCountryStats
+import com.jaimegc.covid19tracker.domain.usecase.GetRegion
+import com.jaimegc.covid19tracker.domain.usecase.GetRegionStats
+import com.jaimegc.covid19tracker.domain.usecase.GetSubRegionStats
+import com.jaimegc.covid19tracker.ui.base.states.PlaceStateScreen
+import com.jaimegc.covid19tracker.ui.base.states.ScreenState
+import com.jaimegc.covid19tracker.ui.country.CountryViewModel
+import com.jaimegc.covid19tracker.utils.MainCoroutineRule
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateErrorDatabaseEmpty
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListCountryStatsLoading
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListCountrySuccess
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListRegionEmptySuccess
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListRegionStatsLoading
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListRegionSuccess
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateScreenErrorDatabaseEmptyData
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateScreenListCountrySuccessData
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateScreenListRegionEmptySuccessData
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateScreenListRegionSuccessData
+import com.jaimegc.covid19tracker.utils.getOrAwaitValue
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+class CountryViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScope = MainCoroutineRule()
+
+    private val captor = argumentCaptor<ScreenState<PlaceStateScreen>>()
+
+    private lateinit var countryViewModel: CountryViewModel
+    private val getCountry: GetCountry = mock()
+    private val getCountryStats: GetCountryStats = mock()
+    private val getRegion: GetRegion = mock()
+    private val getRegionStats: GetRegionStats = mock()
+    private val getSubRegionStats: GetSubRegionStats = mock()
+
+    private val stateObserver: Observer<ScreenState<PlaceStateScreen>> = mock()
+
+    @Before
+    fun setup() {
+        countryViewModel =
+            CountryViewModel(getCountry, getCountryStats, getRegion, getRegionStats, getSubRegionStats)
+        countryViewModel.screenState.observeForever(stateObserver)
+    }
+
+    @Test
+    fun `get countries should return loading and success`() {
+        val flow = flow {
+            emit(Either.right(stateListCountryStatsLoading))
+            emit(Either.right(stateListCountrySuccess))
+        }
+
+        whenever(getCountry.getCountries()).thenReturn(flow)
+
+        countryViewModel.getCountries()
+
+        verify(stateObserver, Mockito.times(2)).onChanged(captor.capture())
+
+        val loading = captor.firstValue
+        val success = captor.secondValue
+
+        assertEquals(ScreenState.Loading, loading)
+        assertTrue(success is ScreenState.Render)
+        assertTrue((success as ScreenState.Render).renderState is PlaceStateScreen.SuccessSpinnerCountries)
+        assertEquals(stateScreenListCountrySuccessData,
+            (success.renderState as PlaceStateScreen.SuccessSpinnerCountries).data)
+    }
+
+    /*****************************************************************
+     *  getOrAwaitValue and observeForTesting from Google repository *
+     *****************************************************************/
+
+    @Test
+    fun `get countries should return loading and success using getOrAwaitValue & observeForTesting`() {
+        val flow = flow {
+            emit(Either.right(stateListCountryStatsLoading))
+            delay(10)
+            emit(Either.right(stateListCountrySuccess))
+        }
+
+        whenever(getCountry.getCountries()).thenReturn(flow)
+
+        countryViewModel.getCountries()
+
+        countryViewModel.screenState.getOrAwaitValue {
+            val loading = countryViewModel.screenState.value
+            coroutineScope.advanceUntilIdle()
+            val success = countryViewModel.screenState.value
+
+            assertEquals(ScreenState.Loading, loading)
+            assertTrue(success is ScreenState.Render)
+            assertTrue((success as ScreenState.Render)
+                .renderState is PlaceStateScreen.SuccessSpinnerCountries)
+            assertEquals(stateScreenListCountrySuccessData,
+                (success.renderState as PlaceStateScreen.SuccessSpinnerCountries).data)
+        }
+    }
+
+    /***********************************************************************************************/
+
+    @Test
+    fun `get countries should return loading and error if database is empty`() {
+        val flow = flow {
+            emit(Either.right(stateListCountryStatsLoading))
+            emit(Either.left(stateErrorDatabaseEmpty))
+        }
+
+        whenever(getCountry.getCountries()).thenReturn(flow)
+
+        countryViewModel.getCountries()
+
+        verify(stateObserver, Mockito.times(2)).onChanged(captor.capture())
+
+        val loading = captor.firstValue
+        val error = captor.secondValue
+
+        assertEquals(ScreenState.Loading, loading)
+        assertTrue(error is ScreenState.Error)
+        assertTrue((error as ScreenState.Error).errorState is PlaceStateScreen.SomeError)
+        assertEquals(stateScreenErrorDatabaseEmptyData,
+            (error.errorState as PlaceStateScreen.SomeError).data)
+    }
+
+    @Test
+    fun `get regions by country should return loading and success`() {
+        val flow = flow {
+            emit(Either.right(stateListRegionStatsLoading))
+            emit(Either.right(stateListRegionSuccess))
+        }
+
+        whenever(getRegion.getRegionsByCountry(any())).thenReturn(flow)
+
+        countryViewModel.getRegionsByCountry("id_country")
+
+        verify(stateObserver, Mockito.times(2)).onChanged(captor.capture())
+
+        val loading = captor.firstValue
+        val success = captor.secondValue
+
+        assertEquals(ScreenState.Loading, loading)
+        assertTrue(success is ScreenState.Render)
+        assertTrue((success as ScreenState.Render).renderState is PlaceStateScreen.SuccessSpinnerRegions)
+        assertEquals(stateScreenListRegionSuccessData,
+            (success.renderState as PlaceStateScreen.SuccessSpinnerRegions).data)
+    }
+
+    @Test
+    fun `get regions by country with empty data should return loading and success`() {
+        val flow = flow {
+            emit(Either.right(stateListRegionStatsLoading))
+            emit(Either.right(stateListRegionEmptySuccess))
+        }
+
+        whenever(getRegion.getRegionsByCountry(any())).thenReturn(flow)
+
+        countryViewModel.getRegionsByCountry("id_country")
+
+        verify(stateObserver, Mockito.times(2)).onChanged(captor.capture())
+
+        val loading = captor.firstValue
+        val success = captor.secondValue
+
+        assertEquals(ScreenState.Loading, loading)
+        assertTrue(success is ScreenState.Render)
+        assertTrue((success as ScreenState.Render).renderState is PlaceStateScreen.SuccessSpinnerRegions)
+        assertEquals(stateScreenListRegionEmptySuccessData,
+            (success.renderState as PlaceStateScreen.SuccessSpinnerRegions).data)
+    }
+}
