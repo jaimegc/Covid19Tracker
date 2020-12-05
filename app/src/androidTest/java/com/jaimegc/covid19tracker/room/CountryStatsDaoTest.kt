8922085d7d409214f5f6b39c11e50a8da5ffa271
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-12-05 19:44:26 +0100

    Add isValid assertions

--- app/src/androidTest/java/com/jaimegc/covid19tracker/room/CountryStatsDaoTest.kt
+++ app/src/androidTest/java/com/jaimegc/covid19tracker/room/CountryStatsDaoTest.kt
@@ -21,7 +21,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CountryStatsDaoTest : DatabaseTest() {
 
-    private lateinit var countryStats: CountryStatsDao
+    private lateinit var countryStatsDao: CountryStatsDao
 
     private lateinit var germanyPojo: CountryAndOneStatsPojo
     private lateinit var francePojo: CountryAndOneStatsPojo
@@ -32,7 +32,7 @@ class CountryStatsDaoTest : DatabaseTest() {
     @Before
     fun setup() {
         initDb()
-        countryStats = database.countryStatsDao()
+        countryStatsDao = database.countryStatsDao()
     }
 
     @After
@@ -40,14 +40,14 @@ class CountryStatsDaoTest : DatabaseTest() {
 
     @Test
     fun getAll_shouldReturnCountries() = runBlocking {
-        countryStats.getAll().take(1).collect {
+        countryStatsDao.getAll().take(1).collect {
             assertThat(it).isEqualTo(listCountryStatsEntity)
         }
     }
 
     @Test
     fun getById_shouldReturnStatsByCountry() = runBlocking {
-        countryStats.getById(
+        countryStatsDao.getById(
             country.id
         ).take(1).collect {
             assertThat(it).isEqualTo(listCountryStatsEntity)
@@ -55,108 +55,135 @@ class CountryStatsDaoTest : DatabaseTest() {
     }
 
     @Test
-    fun getCountryAndStatsByLastDateByCountry_shouldReturnPojo() = runBlocking {
-        countryStats.getCountryAndStatsByLastDate(
+    fun getCountryAndStatsByLastDateByCountry_shouldReturnCountryAndOneStatsPojo() = runBlocking {
+        countryStatsDao.getCountryAndStatsByLastDate(
             country.id
         ).take(1).collect {
             assertThat(it.country).isEqualTo(spainPojo.country)
             assertThat(it.countryStats).isEqualTo(spainPojo.countryStats)
+            assertThat(it.isValid()).isEqualTo(spainPojo.isValid())
         }
     }
 
     @Test
-    fun getCountryAndStatsByCountryAndDate_shouldReturnPojo() = runBlocking {
-        countryStats.getCountryAndStatsByDate(
+    fun getCountryAndStatsByDate_shouldReturnCountryAndOneStatsPojo() = runBlocking {
+        countryStatsDao.getCountryAndStatsByDate(
             country.id,
             spainPojo.countryStats!!.dateTimestamp
         ).take(1).collect {
             assertThat(it.country).isEqualTo(spainPojo.country)
             assertThat(it.countryStats).isEqualTo(spainPojo.countryStats)
+            assertThat(it.isValid()).isEqualTo(spainPojo.isValid())
         }
     }
 
     @Test
     fun getCountriesAndStatsWithMostConfirmed_shouldReturnFiveCountriesMostConfirmedOrderedById() = runBlocking {
-        generateCountryAndStats()
+        generateCountriesAndStats()
 
-        countryStats.getCountriesAndStatsWithMostConfirmed().take(1).collect {
+        countryStatsDao.getCountriesAndStatsWithMostConfirmed().take(1).collect {
             assertThat(it).hasSize(5)
             assertThat(it[0].country).isEqualTo(australiaPojo.country)
             assertThat(it[0].countryStats).isEqualTo(australiaPojo.countryStats)
+            assertThat(it[0].isValid()).isEqualTo(australiaPojo.isValid())
             assertThat(it[1].country).isEqualTo(francePojo.country)
             assertThat(it[1].countryStats).isEqualTo(francePojo.countryStats)
+            assertThat(it[1].isValid()).isEqualTo(francePojo.isValid())
             assertThat(it[2].country).isEqualTo(germanyPojo.country)
             assertThat(it[2].countryStats).isEqualTo(germanyPojo.countryStats)
+            assertThat(it[2].isValid()).isEqualTo(germanyPojo.isValid())
             assertThat(it[3].country).isEqualTo(italyPojo.country)
             assertThat(it[3].countryStats).isEqualTo(italyPojo.countryStats)
+            assertThat(it[3].isValid()).isEqualTo(italyPojo.isValid())
             assertThat(it[4].country).isEqualTo(spainPojo.country)
             assertThat(it[4].countryStats).isEqualTo(spainPojo.countryStats)
+            assertThat(it[4].isValid()).isEqualTo(spainPojo.isValid())
         }
     }
 
     @Test
     fun getCountriesAndStatsWithMostDeaths_shouldReturnFiveCountriesMostDeathsOrderedById() = runBlocking {
-        generateCountryAndStats()
+        generateCountriesAndStats()
 
-        countryStats.getCountriesAndStatsWithMostDeaths().take(1).collect {
+        countryStatsDao.getCountriesAndStatsWithMostDeaths().take(1).collect {
             assertThat(it).hasSize(5)
             assertThat(it[0].country).isEqualTo(australiaPojo.country)
             assertThat(it[0].countryStats).isEqualTo(australiaPojo.countryStats)
+            assertThat(it[0].isValid()).isEqualTo(australiaPojo.isValid())
             assertThat(it[1].country).isEqualTo(egyptPojo.country)
             assertThat(it[1].countryStats).isEqualTo(egyptPojo.countryStats)
+            assertThat(it[1].isValid()).isEqualTo(egyptPojo.isValid())
             assertThat(it[2].country).isEqualTo(francePojo.country)
             assertThat(it[2].countryStats).isEqualTo(francePojo.countryStats)
+            assertThat(it[2].isValid()).isEqualTo(francePojo.isValid())
             assertThat(it[3].country).isEqualTo(germanyPojo.country)
             assertThat(it[3].countryStats).isEqualTo(germanyPojo.countryStats)
+            assertThat(it[3].isValid()).isEqualTo(germanyPojo.isValid())
             assertThat(it[4].country).isEqualTo(italyPojo.country)
             assertThat(it[4].countryStats).isEqualTo(italyPojo.countryStats)
+            assertThat(it[4].isValid()).isEqualTo(italyPojo.isValid())
         }
     }
 
     @Test
     fun getCountriesAndStatsWithMostOpenCases_shouldReturnFiveCountriesMostOpenCasesOrderedById() = runBlocking {
-        generateCountryAndStats()
+        generateCountriesAndStats()
 
-        countryStats.getCountriesAndStatsWithMostOpenCases().take(1).collect {
+        countryStatsDao.getCountriesAndStatsWithMostOpenCases().take(1).collect {
             assertThat(it).hasSize(5)
             assertThat(it[0].country).isEqualTo(australiaPojo.country)
             assertThat(it[0].countryStats).isEqualTo(australiaPojo.countryStats)
+            assertThat(it[0].isValid()).isEqualTo(australiaPojo.isValid())
             assertThat(it[1].country).isEqualTo(francePojo.country)
             assertThat(it[1].countryStats).isEqualTo(francePojo.countryStats)
+            assertThat(it[1].isValid()).isEqualTo(francePojo.isValid())
             assertThat(it[2].country).isEqualTo(germanyPojo.country)
             assertThat(it[2].countryStats).isEqualTo(germanyPojo.countryStats)
+            assertThat(it[2].isValid()).isEqualTo(germanyPojo.isValid())
             assertThat(it[3].country).isEqualTo(italyPojo.country)
             assertThat(it[3].countryStats).isEqualTo(italyPojo.countryStats)
+            assertThat(it[3].isValid()).isEqualTo(italyPojo.isValid())
             assertThat(it[4].country).isEqualTo(spainPojo.country)
             assertThat(it[4].countryStats).isEqualTo(spainPojo.countryStats)
+            assertThat(it[4].isValid()).isEqualTo(spainPojo.isValid())
         }
     }
 
     @Test
     fun getCountriesAndStatsWithMostRecovered_shouldReturnFiveCountriesMostRecoveredOrderedById() = runBlocking {
-        generateCountryAndStats()
+        generateCountriesAndStats()
 
-        countryStats.getCountriesAndStatsWithMostRecovered().take(1).collect {
+        countryStatsDao.getCountriesAndStatsWithMostRecovered().take(1).collect {
             assertThat(it).hasSize(5)
             assertThat(it[0].country).isEqualTo(australiaPojo.country)
             assertThat(it[0].countryStats).isEqualTo(australiaPojo.countryStats)
+            assertThat(it[0].isValid()).isEqualTo(australiaPojo.isValid())
             assertThat(it[1].country).isEqualTo(egyptPojo.country)
             assertThat(it[1].countryStats).isEqualTo(egyptPojo.countryStats)
+            assertThat(it[1].isValid()).isEqualTo(egyptPojo.isValid())
             assertThat(it[2].country).isEqualTo(francePojo.country)
             assertThat(it[2].countryStats).isEqualTo(francePojo.countryStats)
+            assertThat(it[2].isValid()).isEqualTo(francePojo.isValid())
             assertThat(it[3].country).isEqualTo(italyPojo.country)
             assertThat(it[3].countryStats).isEqualTo(italyPojo.countryStats)
+            assertThat(it[3].isValid()).isEqualTo(italyPojo.isValid())
             assertThat(it[4].country).isEqualTo(spainPojo.country)
             assertThat(it[4].countryStats).isEqualTo(spainPojo.countryStats)
+            assertThat(it[4].isValid()).isEqualTo(spainPojo.isValid())
         }
     }
 
-    private fun generateCountryAndStats() = runBlocking {
-        val germany = countryEntity.copy(id = "germany", name = "Germany", nameEs = "Alemania", "DE")
-        val france = countryEntity.copy(id = "france", name = "France", nameEs = "Francia", code = "FR")
-        val italy = countryEntity.copy(id = "italy", name = "Italy", nameEs = "Italia", code = "IT")
-        val australia = countryEntity.copy(id = "australia", name = "Australia", nameEs = "Australia", code = "AU")
-        val egypt = countryEntity.copy(id = "egypt", name = "Egypt", nameEs = "Egipto", code = "EG")
+    private fun generateCountriesAndStats() = runBlocking {
+        val germany =
+            countryEntity.copy(id = "germany", name = "Germany", nameEs = "Alemania", code = "DE")
+        val france =
+            countryEntity.copy(id = "france", name = "France", nameEs = "Francia", code = "FR")
+        val italy =
+            countryEntity.copy(id = "italy", name = "Italy", nameEs = "Italia", code = "IT")
+        val australia =
+            countryEntity.copy(id = "australia", name = "Australia", nameEs = "Australia", code = "AU")
+        val egypt =
+            countryEntity.copy(id = "egypt", name = "Egypt", nameEs = "Egipto", code = "EG")
 
         val germanyStats = countryStatsEntity.copy(
             idCountryFk = germany.id,
