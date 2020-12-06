e2013492bb09ccb9315d6ef45c96fa5b66cfcb34
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-12-06 13:22:08 +0100

    Add generic use case class for tests

---
+++ app/src/test/java/com/jaimegc/covid19tracker/utils/UseCaseTest.kt
@@ -0,0 +1,25 @@
+package com.jaimegc.covid19tracker.utils
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.jaimegc.covid19tracker.data.repository.CovidTrackerRepository
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
+import org.junit.Rule
+
+abstract class UseCaseTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val coroutineScope = MainCoroutineRule()
+
+    @MockK
+    internal lateinit var repository: CovidTrackerRepository
+
+    @Before
+    fun init() {
+        MockKAnnotations.init(this)
+    }
+}
