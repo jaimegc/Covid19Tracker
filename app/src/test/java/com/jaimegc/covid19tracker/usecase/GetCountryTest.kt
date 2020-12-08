b3208dc29243335f3306fb012fe042035e95cc65
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-12-08 14:59:38 +0100

    Add database unknown error test

--- app/src/test/java/com/jaimegc/covid19tracker/usecase/GetCountryTest.kt
+++ app/src/test/java/com/jaimegc/covid19tracker/usecase/GetCountryTest.kt
@@ -52,26 +52,6 @@ class GetCountryTest : UseCaseTest() {
     }
 
     @Test
-    fun `get countries should return loading and unknown database error`() = runBlockingTest {
-        val flow = flow {
-            emit(Either.right(stateListCountryLoading))
-            emit(Either.left(stateErrorUnknownDatabase))
-        }
-
-        every { repository.getCountries() } returns flow
-
-        val flowUseCase = getCountry.getCountries()
-
-        verify { repository.getCountries() }
-        flowUseCase.collectIndexed { index, data ->
-            when (index) {
-                0 -> assertThat(data).isEqualTo(Either.right(stateListCountryLoading))
-                1 -> assertThat(data).isEqualTo(Either.left(stateErrorUnknownDatabase))
-            }
-        }
-    }
-
-    @Test
     fun `get countries with empty data should return loading and empty success`() = runBlockingTest {
         val flow = flow {
             emit(Either.right(stateListCountryLoading))
@@ -87,6 +67,26 @@ class GetCountryTest : UseCaseTest() {
             when (index) {
                 0 -> assertThat(data).isEqualTo(Either.right(stateListCountryLoading))
                 1 -> assertThat(data).isEqualTo(Either.right(stateListCountryEmptyData))
+            }
+        }
+    }
+
+    @Test
+    fun `get countries with database problem should return loading and unknown database error`() = runBlockingTest {
+        val flow = flow {
+            emit(Either.right(stateListCountryLoading))
+            emit(Either.left(stateErrorUnknownDatabase))
+        }
+
+        every { repository.getCountries() } returns flow
+
+        val flowUseCase = getCountry.getCountries()
+
+        verify { repository.getCountries() }
+        flowUseCase.collectIndexed { index, data ->
+            when (index) {
+                0 -> assertThat(data).isEqualTo(Either.right(stateListCountryLoading))
+                1 -> assertThat(data).isEqualTo(Either.left(stateErrorUnknownDatabase))
             }
         }
     }
@@ -115,26 +115,7 @@ class GetCountryTest : UseCaseTest() {
     }
 
     @Test
-    fun `get countries should return loading and unknown database error using flow test`() = runBlockingTest {
-        val flow = flow {
-            emit(Either.right(stateListCountryLoading))
-            emit(Either.left(stateErrorUnknownDatabase))
-        }
-
-        every { repository.getCountries() } returns flow
-
-        val flowUseCase = getCountry.getCountries()
-
-        verify { repository.getCountries() }
-        flowUseCase.test(this) {
-            assertValues(Either.right(stateListCountryLoading), Either.left(stateErrorUnknownDatabase))
-            assertValueCount(2)
-            assertComplete()
-        }
-    }
-
-    @Test
-    fun `get countries should return loading and empty success using flow test`() = runBlockingTest {
+    fun `get countries with empty data should return loading and empty success using flow test`() = runBlockingTest {
         val flow = flow {
             emit(Either.right(stateListCountryLoading))
             emit(Either.right(stateListCountryEmptyData))
@@ -147,6 +128,25 @@ class GetCountryTest : UseCaseTest() {
         verify { repository.getCountries() }
         flowUseCase.test(this) {
             assertValues(Either.right(stateListCountryLoading), Either.right(stateListCountryEmptyData))
+            assertValueCount(2)
+            assertComplete()
+        }
+    }
+
+    @Test
+    fun `get countries with database problem should return loading and unknown database error using flow test`() = runBlockingTest {
+        val flow = flow {
+            emit(Either.right(stateListCountryLoading))
+            emit(Either.left(stateErrorUnknownDatabase))
+        }
+
+        every { repository.getCountries() } returns flow
+
+        val flowUseCase = getCountry.getCountries()
+
+        verify { repository.getCountries() }
+        flowUseCase.test(this) {
+            assertValues(Either.right(stateListCountryLoading), Either.left(stateErrorUnknownDatabase))
             assertValueCount(2)
             assertComplete()
         }
