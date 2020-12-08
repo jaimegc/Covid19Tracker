5f2697d7b51a9d46aa4500373eec290489eb68bf
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-12-08 14:59:57 +0100

    Add database unknown error test

--- app/src/test/java/com/jaimegc/covid19tracker/usecase/GetWorldStatsTest.kt
+++ app/src/test/java/com/jaimegc/covid19tracker/usecase/GetWorldStatsTest.kt
@@ -3,7 +3,8 @@ package com.jaimegc.covid19tracker.usecase
 import arrow.core.Either
 import com.google.common.truth.Truth.assertThat
 import com.jaimegc.covid19tracker.domain.usecase.GetWorldStats
-import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateErrorDatabaseEmpty
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateErrorUnknownDatabase
+import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListWorldStatsEmptyData
 import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListWorldStatsLoading
 import com.jaimegc.covid19tracker.utils.ScreenStateBuilder.stateListWorldStatsSuccess
 import com.jaimegc.covid19tracker.utils.UseCaseTest
@@ -51,10 +52,10 @@ class GetWorldStatsTest : UseCaseTest() {
     }
 
     @Test
-    fun `get world all stats should return loading and error database empty if date doesnt exist`() = runBlockingTest {
+    fun `get world all stats with empty data should return loading and empty success`() = runBlockingTest {
         val flow = flow {
             emit(Either.right(stateListWorldStatsLoading))
-            emit(Either.left(stateErrorDatabaseEmpty))
+            emit(Either.left(stateErrorUnknownDatabase))
         }
 
         every { repository.getWorldAllStats() } returns flow
@@ -65,7 +66,27 @@ class GetWorldStatsTest : UseCaseTest() {
         flowUseCase.collectIndexed { index, data ->
             when (index) {
                 0 -> assertThat(data).isEqualTo(Either.right(stateListWorldStatsLoading))
-                1 -> assertThat(data).isEqualTo(Either.left(stateErrorDatabaseEmpty))
+                1 -> assertThat(data).isEqualTo(Either.left(stateErrorUnknownDatabase))
+            }
+        }
+    }
+
+    @Test
+    fun `get world all stats should with database problem return loading and unknown database error`() = runBlockingTest {
+        val flow = flow {
+            emit(Either.right(stateListWorldStatsLoading))
+            emit(Either.left(stateErrorUnknownDatabase))
+        }
+
+        every { repository.getWorldAllStats() } returns flow
+
+        val flowUseCase = getWorldStats.getWorldAllStats()
+
+        verify { repository.getWorldAllStats() }
+        flowUseCase.collectIndexed { index, data ->
+            when (index) {
+                0 -> assertThat(data).isEqualTo(Either.right(stateListWorldStatsLoading))
+                1 -> assertThat(data).isEqualTo(Either.left(stateErrorUnknownDatabase))
             }
         }
     }
@@ -94,10 +115,10 @@ class GetWorldStatsTest : UseCaseTest() {
     }
 
     @Test
-    fun `get world all stats should return loading and error database empty if date doesnt exist using flow test`() = runBlockingTest {
+    fun `get world all stats with empty data should return loading and empty success using flow test`() = runBlockingTest {
         val flow = flow {
             emit(Either.right(stateListWorldStatsLoading))
-            emit(Either.left(stateErrorDatabaseEmpty))
+            emit(Either.right(stateListWorldStatsEmptyData))
         }
 
         every { repository.getWorldAllStats() } returns flow
@@ -106,7 +127,26 @@ class GetWorldStatsTest : UseCaseTest() {
 
         verify { repository.getWorldAllStats() }
         flowUseCase.test(this) {
-            assertValues(Either.right(stateListWorldStatsLoading), Either.left(stateErrorDatabaseEmpty))
+            assertValues(Either.right(stateListWorldStatsLoading), Either.right(stateListWorldStatsEmptyData))
+            assertValueCount(2)
+            assertComplete()
+        }
+    }
+
+    @Test
+    fun `get world all stats should with database problem return loading and unknown database error using flow test`() = runBlockingTest {
+        val flow = flow {
+            emit(Either.right(stateListWorldStatsLoading))
+            emit(Either.left(stateErrorUnknownDatabase))
+        }
+
+        every { repository.getWorldAllStats() } returns flow
+
+        val flowUseCase = getWorldStats.getWorldAllStats()
+
+        verify { repository.getWorldAllStats() }
+        flowUseCase.test(this) {
+            assertValues(Either.right(stateListWorldStatsLoading), Either.left(stateErrorUnknownDatabase))
             assertValueCount(2)
             assertComplete()
         }
