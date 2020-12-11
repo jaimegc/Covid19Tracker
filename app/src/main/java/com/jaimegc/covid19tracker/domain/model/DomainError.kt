f733e443df52574b9f31824f0ff1faca6a60addb
Jaime Guerrero Cubero <jguerrero@carto.com>
2020-12-11 14:07:56 +0100

    Rename api & domain errors

--- app/src/main/java/com/jaimegc/covid19tracker/domain/model/DomainError.kt
+++ app/src/main/java/com/jaimegc/covid19tracker/domain/model/DomainError.kt
@@ -1,13 +1,13 @@
 package com.jaimegc.covid19tracker.domain.model
 
 sealed class DomainError {
-    object NoInternetDomainError : DomainError()
+    object NoInternetError : DomainError()
     object SocketTimeoutError : DomainError()
     object GenericDomainError : DomainError()
     object ServerDomainError : DomainError()
     object ServerDataDomainError : DomainError()
     object ServerForbiddenDomainError : DomainError()
-    data class UnknownDomainError(val errorMessage: String = "Unknown Error") : DomainError()
+    data class MapperDomainError(val errorMessage: String = "Unknown Error") : DomainError()
     data class UnknownDatabaseError(val errorMessage: String = "Database Error") : DomainError()
     object DatabaseEmptyData : DomainError()
 }
