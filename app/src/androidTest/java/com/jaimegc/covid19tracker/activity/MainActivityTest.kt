ad1983e4cd19394ae07ae11f2ebace05f734a4aa
Jaime Guerrero Cubero <jguerrero@carto.com>
2021-01-16 16:15:47 +0100

    Add get covid tracker test

--- app/src/androidTest/java/com/jaimegc/covid19tracker/activity/MainActivityTest.kt
+++ app/src/androidTest/java/com/jaimegc/covid19tracker/activity/MainActivityTest.kt
@@ -11,27 +11,54 @@ import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.jaimegc.covid19tracker.R
 import com.jaimegc.covid19tracker.ui.home.MainActivity
 import com.jaimegc.covid19tracker.ui.world.WorldFragment
-import com.jaimegc.covid19tracker.utils.UITest
 import com.jaimegc.covid19tracker.utils.matchers.BottomNavigationViewMenuItemMatcher.Companion.bottomNavigationViewHasMenuItemChecked
 import com.google.common.truth.Truth.assertThat
+import com.jaimegc.covid19tracker.data.room.Covid19TrackerDatabase
 import com.jaimegc.covid19tracker.ui.country.CountryFragment
-import org.junit.After
+import com.jaimegc.covid19tracker.ui.home.MainViewModel
+import io.mockk.mockk
+import io.mockk.mockkClass
+import io.mockk.verify
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.mock.MockProviderRule
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class MainActivityTest : UITest() {
+class MainActivityTest : KoinTest {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
+    private val viewModel = mockk<MainViewModel>(relaxed = true)
+    private lateinit var mockModule: Module
+
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        mockkClass(clazz)
+    }
 
     @Before
     fun setUp() {
-        scenario = ActivityScenario.launch(MainActivity::class.java)
+        mockModule = module(createdAtStart = true, override = true) {
+            single {
+                Covid19TrackerDatabase.buildTest(get())
+            }
+            single {
+                viewModel
+            }
+        }
     }
 
     @Test
     fun openActivity_shouldShowCountryFragmentByDefault() {
+        loadKoinModules(mockModule)
+
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+
         scenario.onActivity { activity ->
             val fragments =
                 activity.supportFragmentManager.fragments.first().childFragmentManager.fragments
@@ -42,15 +69,16 @@ class MainActivityTest : UITest() {
         onView(withId(R.id.nav_view)).check(
             matches(bottomNavigationViewHasMenuItemChecked(R.id.navigation_country))
         )
-    }
 
-    @After
-    fun tearDown() {
         scenario.close()
     }
 
     @Test
     fun clickOnNavigationWorld_shouldShowWorldFragment() {
+        loadKoinModules(mockModule)
+
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+
         onView(withId(R.id.navigation_world)).perform(click())
 
         scenario.onActivity { activity ->
@@ -63,15 +91,34 @@ class MainActivityTest : UITest() {
         onView(withId(R.id.nav_view)).check(
             matches(bottomNavigationViewHasMenuItemChecked(R.id.navigation_world))
         )
+
+        scenario.close()
     }
 
     @Test
     fun clickOnBackButtonInNavigationWorld_shouldShowNavigationCountry() {
+        loadKoinModules(mockModule)
+
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+
         onView(withId(R.id.navigation_world)).perform(click())
         onView(isRoot()).perform(pressBack())
 
         onView(withId(R.id.nav_view)).check(
             matches(bottomNavigationViewHasMenuItemChecked(R.id.navigation_country))
         )
+
+        scenario.close()
+    }
+
+    @Test
+    fun openActivity_shouldCallGetCovidTracker() {
+        loadKoinModules(mockModule)
+
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+
+        verify { viewModel.getCovidTracker() }
+
+        scenario.close()
     }
 }
