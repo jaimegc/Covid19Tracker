ac97beff2432f69ee7916431013ffcba8c0b8644
Jaime Guerrero Cubero <jguerrero@carto.com>
2021-01-23 15:11:16 +0100

    Add bases tests for robolectric

---
+++ app/src/test/java/com/jaimegc/covid19tracker/utils/UIRobolectricTest.kt
@@ -0,0 +1,93 @@
+package com.jaimegc.covid19tracker.utils
+
+import arrow.core.Either
+import com.jaimegc.covid19tracker.ModelFactoryTest.countryEmptyRegionsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.countryEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.countryStatsEmptyRegionsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.countryStatsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.regionEmptyRegionsStatsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.regionEmptySubRegionsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.regionEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.regionStatsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.subRegionEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.subRegionStatsEntity
+import com.jaimegc.covid19tracker.ModelFactoryTest.worldStatsEntity
+import com.jaimegc.covid19tracker.ScreenStateFactoryTest.stateCovidTrackerEmptyData
+import com.jaimegc.covid19tracker.data.room.Covid19TrackerDatabase
+import com.jaimegc.covid19tracker.data.room.daos.CovidTrackerDao
+import com.jaimegc.covid19tracker.domain.usecase.GetCovidTracker
+import io.mockk.every
+import io.mockk.mockkClass
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.unloadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import org.koin.test.mock.MockProviderRule
+import org.koin.test.mock.declareMock
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.LooperMode
+
+@RunWith(RobolectricTestRunner::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+abstract class UIRobolectricTest : KoinTest, SharedPreferencesRobolectricTest() {
+    private lateinit var mockModule: Module
+    private val covidTrackerDao: CovidTrackerDao by inject()
+
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        mockkClass(clazz)
+    }
+
+    @Before
+    fun before() {
+        // This is not necessary because we have the Covid19TrackerTestRunner
+        // with the databaseTestModule using this. It's only an example :)
+        mockModule = module(createdAtStart = true, override = true) {
+            single {
+                Covid19TrackerDatabase.buildTest(get())
+            }
+        }
+
+        loadKoinModules(mockModule)
+
+        mockRequests()
+
+        runBlocking {
+            covidTrackerDao.populateDatabase(
+                worldsStats = listOf(worldStatsEntity),
+                countries = listOf(countryEntity, countryEmptyRegionsEntity),
+                countriesStats = listOf(countryStatsEntity, countryStatsEmptyRegionsEntity),
+                regions = listOf(regionEntity, regionEmptySubRegionsEntity),
+                regionsStats = listOf(regionStatsEntity, regionEmptyRegionsStatsEntity),
+                subRegions = listOf(subRegionEntity),
+                subRegionsStats = listOf(subRegionStatsEntity),
+            )
+        }
+    }
+
+    @After
+    fun after() {
+        unloadKoinModules(mockModule)
+    }
+
+    private fun mockRequests() {
+        // Empty requests for UpdateDatabaseWorker
+        declareMock<FileUtils> {
+            every { generateCurrentDates() } returns listOf()
+        }
+
+        declareMock<GetCovidTracker> {
+            every { getCovidTrackerByDate(any()) } returns flow {
+                emit(Either.right(stateCovidTrackerEmptyData))
+            }
+        }
+    }
+}
